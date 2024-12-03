d817efe92e6585c5e97d9b6137e240c2812b5a74
Peter Kalverla <peter.kalverla@gmx.com>
2024-12-03 11:06:06 +0100

    Add tile templates and change the geometry from points to tiles

--- extract_area.py
+++ extract_area.py
@@ -1,6 +1,7 @@
 import numpy as np
 import rasterio
 import geopandas as gpd
+from shapely.affinity import translate
 
 LCZ_HEIGHTS = {
     51: 37.5,
@@ -15,6 +16,46 @@ LCZ_HEIGHTS = {
     60: 10.0,
     61: 10.0,
 }
+
+_tile1 = gpd.read_file("lcz_tiles/tile_1_wgs84_51_52_53.gpkg").geometry.item()
+_tile2 = gpd.read_file("lcz_tiles/tile_2_wgs84_54_55_56.gpkg").geometry.item()
+_tile3 = gpd.read_file("lcz_tiles/tile_3_wgs84_58.gpkg").geometry.item()
+_tile4 = gpd.read_file("lcz_tiles/tile_4_wgs84_59.gpkg").geometry.item()
+_tile5 = gpd.read_file("lcz_tiles/tile_5_wgs84_60.gpkg").geometry.item()
+LCZ_MULTIPOLYGONS = {
+    51: _tile1,
+    52: _tile1,
+    53: _tile1,
+    54: _tile2,
+    55: _tile2,
+    56: _tile2,
+    # 57: _tile3,  # Haven't needed it yet
+    58: _tile3,
+    59: _tile4,
+    60: _tile5,
+    # 61: _tile6,  # Haven't needed it yet
+}
+
+
+def get_tile_at_coords(row):
+    """Look up the tile template and move it to the given coordinate.
+
+    Input is a row of a geopandas dataframe with columns "LCZ" and point
+    geometry. The points should represent grid cell centers.
+    """
+    # Extract information from dataframe row
+    lcz_type = row["LCZ"]
+    x = row.geometry.x
+    y = row.geometry.y
+
+    # Look up the tile template corresonding to the LCZ class
+    tile = LCZ_MULTIPOLYGONS.get(lcz_type)
+    tile_x = tile.centroid.x
+    tile_y = tile.centroid.y
+
+    # Return a copy of the tile centered on the given coordinate
+    return translate(tile, xoff=(x - tile_x), yoff=(y - tile_y))
+
 
 if __name__ == "__main__":
     # http://bboxfinder.com/#52.273620,4.724808,52.458729,5.182114
@@ -44,9 +85,15 @@ if __name__ == "__main__":
             columns=["LCZ"],
         )
 
+        # Discard non-urban landuse classes
+        gdf = gdf.where(gdf["LCZ"] > 50).dropna()
+
         # Add height column
         gdf["height"] = gdf["LCZ"].map(LCZ_HEIGHTS).fillna(0)
-        quit()
+
+        # Add polygons
+        new_geometry = gdf.apply(get_tile_at_coords, axis=1)
+        gdf.geometry = new_geometry
 
         # Save
         gdf.to_file("lcz_subset.gpkg", driver="GPKG")
