d6330153ece1a1b2770f12de3eadc8c4430c04d4
Maurice de Kleijn <mtm.de.kleijn@gmail.com>
2024-12-03 20:31:37 +0100

    added tile for lcz 57 and renamed other tiles (#7)

--- src/cityblocks/_core.py
+++ src/cityblocks/_core.py
@@ -22,9 +22,10 @@ LCZ_HEIGHTS = {
 tiles = files("cityblocks")
 _tile1 = gpd.read_file(f"{tiles}/tile_1_wgs84_51_52_53.gpkg").geometry.item()
 _tile2 = gpd.read_file(f"{tiles}/tile_2_wgs84_54_55_56.gpkg").geometry.item()
-_tile3 = gpd.read_file(f"{tiles}/tile_3_wgs84_58.gpkg").geometry.item()
-_tile4 = gpd.read_file(f"{tiles}/tile_4_wgs84_59.gpkg").geometry.item()
-_tile5 = gpd.read_file(f"{tiles}/tile_5_wgs84_60.gpkg").geometry.item()
+_tile3 = gpd.read_file(f"{tiles}/tile_3_wgs84_57.gpkg").geometry.item()
+_tile4 = gpd.read_file(f"{tiles}/tile_4_wgs84_58.gpkg").geometry.item()
+_tile5 = gpd.read_file(f"{tiles}/tile_5_wgs84_59.gpkg").geometry.item()
+_tile6 = gpd.read_file(f"{tiles}/tile_6_wgs84_60.gpkg").geometry.item()
 LCZ_MULTIPOLYGONS = {
     51: _tile1,
     52: _tile1,
@@ -32,11 +33,11 @@ LCZ_MULTIPOLYGONS = {
     54: _tile2,
     55: _tile2,
     56: _tile2,
-    # 57: _tile3,  # Haven't needed it yet
-    58: _tile3,
-    59: _tile4,
-    60: _tile5,
-    # 61: _tile6,  # Haven't needed it yet
+    57: _tile3,  
+    58: _tile4,
+    59: _tile5,
+    60: _tile6,
+    # 61: _tile7,  # Haven't needed it yet
 }
 
 
